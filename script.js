db2c7784fbe123aab0efeedde79b9974bf11fbfa
Nícolas Cheneaux <nicolas.farfan.ch@usach.cl>
2022-02-03 01:24:21 -0300

    Varios avances Se implementa las funciones de deleteAll nodes y deleteNode, ademas se cambia el estilo de los botones y nodos, y se corrijen bugs

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+// Elements
 const $circle = document.querySelector(".crcl")
 const $arrow = document.querySelector(".arrow")
-const $button = document.querySelector("#button")
+// Buttons
+const $add = document.querySelector("#add")
+const $delete = document.querySelector("#delete")
+const $deleteAll = document.querySelector("#deleteAll")
+// Inputs, Containers
 const $container = document.querySelector("#container")
 const $inputNumber = document.querySelector("#inputNumber")
 const $error = document.querySelector("#error")
-const time = async () =>{
-  await new Promise(resolve => setTimeout(resolve, 500))}
 
+const time = async (time = 500) =>{
+  await new Promise(resolve => setTimeout(resolve, time))
+}
+// Animaciones de los nodos y las flechas
 const rotateArrow = (el,time,deg) =>{
   el.animate([
     {transform: `rotate(${-deg-30}deg)`},
@@ -23,28 +30,35 @@ const animateCircle = (el,time,deg) =>{
     duration: time,
   })
 }
+// Lectura de input
 const readInputs = () =>{
-
   if($inputNumber.value === ""){
+    $error.classList.remove("d-none")
     return 0
   }
-  if($inputNumber.value>= 0 && $inputNumber.value<=9){
+  if($inputNumber.value.length === 1){
+    $error.classList.add("d-none")
     return $inputNumber.value
   }
+  $error.classList.remove("d-none")
   return 0
 }
-const createCircle = async() =>{
-  if($inputNumber.value>= 0 && $inputNumber.value<=9){
+// Creacion de el nuevo nodo
+const createCircle = async(input) =>{
+  if($inputNumber.value.length === 1){
     const circle = $circle.cloneNode(true)
-    circle.innerHTML = readInputs()
+
     $container.insertAdjacentElement("beforeend",circle)
+    getLastCircle().children.value.innerHTML = input
   }
 }
+// Creacion de la nueva flecha
 const createArrow = async() =>{
   const arrow = $arrow.cloneNode(true)
   $container.insertAdjacentElement("beforeend",arrow)
 }
 
+// Ultimo nodo
 const getLastCircle = () =>{
   const $circ = document.querySelectorAll(".crcl")
   return $circ[$circ.length-1]
@@ -53,18 +67,72 @@ const getLastArrow = () =>{
   const $arr = document.querySelectorAll(".arrow")
   return $arr[$arr.length-1]
 }
-
-const animate = async () =>{
-  if(readInputs() !== 0 ){
-  animateCircle(getLastCircle(),200,150)
+// Animacion de añadir
+const animate = async (circle,arrow) =>{
+  animateCircle(circle,200,150)
   await time()
-  rotateArrow(getLastArrow(),200,20)
+  rotateArrow(arrow,200,20)
+  await time(300)
+}
+// Animacion de eliminacion
+const animateReverse = async (circle,arrow) =>{
+  rotateArrow(arrow,200,20)
   await time()
-  await createCircle()
+  animateCircle(circle,200,150)
+  await time(300)
+}
+// Animacion de crear un nuevo nodo
+const crearNuevo = async (input) =>{
+  await time()
+  await createCircle(input)
   await time()
   await createArrow()
 }
-  return
+// Funcion que anima hasta el final
+const animatAll = async () =>{
+  let i = 0
+  for(e of document.querySelectorAll(".crcl")){
+    await animate(e,document.querySelectorAll(".arrow")[i])
+    i+=1
+  }
+}
+// Crear nuevo nodo
+const nuevoNodo = async () =>{
+  let input = readInputs()
+  if(input !== 0 ){
+    let input = readInputs()
+    await animatAll()
+    await crearNuevo(input)
+  }
+  else{
+    return console.error("ERROR DE INPUT")
+  }
 }
 
-$button.addEventListener('click',animate)
+// Elimina el numero de nodo (OneBase)
+const deleteIndex = async() =>{
+  let i = 0
+  for(e of document.querySelectorAll(".crcl")){
+    let arrow = document.querySelectorAll(".arrow")[i]
+    await animate(e,arrow)
+    if(i === readInputs()-1){
+      e.remove()
+      arrow.remove()
+      return
+    }
+    i+=1
+  }
+}
+const deleteAll = async() =>{
+  for(let i = document.querySelectorAll(".crcl").length; i >= 0;i--){
+    let circle= document.querySelectorAll(".crcl")[i-1]
+    let arrow = document.querySelectorAll(".arrow")[i-1]
+    await animateReverse(circle,arrow)
+    arrow.remove()
+    circle.remove()
+    await time(200)
+  }
+}
+$add.addEventListener('click',nuevoNodo)
+$delete.addEventListener('click',deleteIndex)
+$deleteAll.addEventListener('click',deleteAll)
