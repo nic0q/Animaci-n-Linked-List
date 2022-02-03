c5492edb03f34d6b7b15b203cc69cba5256d1a7c
Nícolas Cheneaux <nicolas.farfan.ch@usach.cl>
2022-02-03 16:48:56 -0300

    Nuevas funcionalidades Se implementa, el borrar por coincidencia del valor del nodo, tambien se corrigen errores con el boton de eliminar todo

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ const $arrow = document.querySelector(".arrow")
 const $add = document.querySelector("#add")
 const $delete = document.querySelector("#delete")
 const $deleteAll = document.querySelector("#deleteAll")
+const $deleteC = document.querySelector("#deleteC")
 // Inputs, Containers
 const $container = document.querySelector("#container")
 const $inputNumber = document.querySelector("#inputNumber")
 const $error = document.querySelector("#error")
+
+const freezeButtons = () =>{
+  $add.classList.add("disable")
+  $delete.classList.add("disable")  
+  $deleteAll.classList.add("disable")
+  $deleteC.classList.add("disable")
+
+}
+
+const unfreezeButtons = () =>{
+  $add.classList.remove("disable")
+  $delete.classList.remove("disable") 
+  $deleteAll.classList.remove("disable")
+  $deleteC.classList.remove("disable")
+}
 
 const time = async (time = 500) =>{
   await new Promise(resolve => setTimeout(resolve, time))
@@ -34,20 +50,19 @@ const animateCircle = (el,time,deg) =>{
 const readInputs = () =>{
   if($inputNumber.value === ""){
     $error.classList.remove("d-none")
-    return 0
+    return -1
   }
   if($inputNumber.value.length === 1){
     $error.classList.add("d-none")
     return $inputNumber.value
   }
   $error.classList.remove("d-none")
-  return 0
+  return -1
 }
 // Creacion de el nuevo nodo
 const createCircle = async(input) =>{
   if($inputNumber.value.length === 1){
     const circle = $circle.cloneNode(true)
-
     $container.insertAdjacentElement("beforeend",circle)
     getLastCircle().children.value.innerHTML = input
   }
@@ -100,9 +115,10 @@ const animatAll = async () =>{
 const nuevoNodo = async () =>{
   let input = readInputs()
   if(input !== 0 ){
-    let input = readInputs()
+    freezeButtons()
     await animatAll()
     await crearNuevo(input)
+    unfreezeButtons()
   }
   else{
     return console.error("ERROR DE INPUT")
@@ -111,6 +127,8 @@ const nuevoNodo = async () =>{
 
 // Elimina el numero de nodo (OneBase)
 const deleteIndex = async() =>{
+  if(readInputs() !== -1){
+  freezeButtons()
   let i = 0
   for(e of document.querySelectorAll(".crcl")){
     let arrow = document.querySelectorAll(".arrow")[i]
@@ -118,13 +136,21 @@ const deleteIndex = async() =>{
     if(i === readInputs()-1){
       e.remove()
       arrow.remove()
+      unfreezeButtons()
       return
     }
     i+=1
   }
+  unfreezeButtons()
 }
+else{
+  return console.error("ERROR DE INPUT")
+}
+}
+
 const deleteAll = async() =>{
-  for(let i = document.querySelectorAll(".crcl").length; i >= 0;i--){
+  freezeButtons()
+  for(let i = document.querySelectorAll(".crcl").length; i > 0;i--){
     let circle= document.querySelectorAll(".crcl")[i-1]
     let arrow = document.querySelectorAll(".arrow")[i-1]
     await animateReverse(circle,arrow)
@@ -132,7 +158,30 @@ const deleteAll = async() =>{
     circle.remove()
     await time(200)
   }
+  unfreezeButtons()
 }
+const deleteCo = async() =>{
+  if($inputNumber !== -1){
+  freezeButtons()
+  let input = readInputs()
+  const cr = document.querySelectorAll(".crcl")
+  const arr = document.querySelectorAll(".arrow")
+  for(let i = 0; i < cr.length;i++){
+    await animate(cr[i],arr[i])
+    if(cr[i].children.value.innerHTML === input){
+      
+      cr[i].remove()
+      arr[i].remove()
+    }
+  }
+  unfreezeButtons()
+}
+else{
+  return console.error("ERROR DE INPUT")
+}
+}
+
 $add.addEventListener('click',nuevoNodo)
 $delete.addEventListener('click',deleteIndex)
 $deleteAll.addEventListener('click',deleteAll)
+$deleteC.addEventListener('click',deleteCo)
